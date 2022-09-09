a39ffda1ed3f866bbc3aa72b4b46c3e681ecbf0c
Daniel <djisenberg2015@gmail.com>
2022-09-09 19:06:23 -0400

    Clamp ball launch angle between 0 and 90 degrees

--- Ball.gd
+++ Ball.gd
@@ -12,8 +12,10 @@ func _process(_delta):
 		print("Angle: " + str(angle) + "\nX component: " + str(_xVector) + "\nY component: " + str(_yVector) + "\n")
 		apply_impulse(Vector2.ZERO, Vector2(magnitude*cos(deg2rad(angle)), magnitude*sin(deg2rad(angle))))
 	elif Input.is_action_pressed("angle_up"):
-		angle -= 1
+		if not angle >= 0:
+			angle += 1
 	elif Input.is_action_pressed("angle_down"):
-		angle += 1
+		if not angle <= -90:
+			angle -= 1
 	if Input.is_action_pressed("display_angle"):
 		self.rotation_degrees = angle

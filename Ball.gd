98218473b1599bda207b16f2a156351ab4525efe
Daniel <djisenberg2015@gmail.com>
2022-09-09 18:19:39 -0400

    Fixed launch vector calculation and added hotkey to display launch angle (f)

--- Ball.gd
+++ Ball.gd
@@ -7,10 +7,13 @@ var _launched := false
 
 func _process(_delta):
 	if not _launched and Input.is_action_just_pressed("launch"):
-		apply_impulse(Vector2.ZERO, Vector2(magnitude*cos(angle), magnitude*sin(-angle)))
+		var _xVector = magnitude * cos(angle)
+		var _yVector = magnitude * sin(angle)
+		print("Angle: " + str(angle) + "\nX component: " + str(_xVector) + "\nY component: " + str(_yVector) + "\n")
+		apply_impulse(Vector2.ZERO, Vector2(magnitude*cos(deg2rad(angle)), magnitude*sin(deg2rad(angle))))
 	elif Input.is_action_pressed("angle_up"):
-		angle += 1
-		self.rotation_degrees = angle
-	elif Input.is_action_pressed("angle_down"):
 		angle -= 1
+	elif Input.is_action_pressed("angle_down"):
+		angle += 1
+	if Input.is_action_pressed("display_angle"):
 		self.rotation_degrees = angle
